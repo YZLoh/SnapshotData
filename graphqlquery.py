09a55c2e1c17d9f34e175b4f808b00a0753d973a
zacyapjq <yapjiaqing@gmail.com>
2022-07-15 00:52:18 +0800

    added votes query

--- graphqlquery.py
+++ graphqlquery.py
@@ -2,8 +2,8 @@ import requests
 import json
 import pandas as pd
 
-query = """ query Proposals {
-  proposals(first: 20, skip: 0, where:{space:"gitcoindao.eth"}, orderBy: "created", orderDirection: desc) {
+ProposalQuery = """ query Proposals {
+  proposals(first: 2, skip: 0, where:{space:"gitcoindao.eth"}, orderBy: "created", orderDirection: desc) {
     id
     title
     body
@@ -21,13 +21,35 @@ query = """ query Proposals {
 }
 """
 
-json_response = requests.get(f'https://hub.snapshot.org/graphql', headers={'Accept': 'application/json'}, json={'query': query})
+json_response = requests.get(f'https://hub.snapshot.org/graphql', headers={'Accept': 'application/json'},
+                             json={'query': ProposalQuery})
 
 print(json_response.status_code)
 print(json_response.text)
 
 json_data = json.loads(json_response.text)
 
-df_data = json_data["data"]["proposals"]
-df = pd.DataFrame(df_data)
-print(df)
+proposals_data = json_data["data"]["proposals"]
+proposals_df = pd.DataFrame(proposals_data)
+print(proposals_df)
+
+for proposal in proposals_data:
+    proposal_id = proposal["id"]
+    VotesQuery = ''' query Votes {
+      votes(first: 1000, where: {proposal: "%s"}) {
+        id
+        voter
+        created
+        choice
+        space {
+          id
+        }
+      }
+    }
+    ''' % proposal_id
+
+    json_response = requests.get(f'https://hub.snapshot.org/graphql', headers={'Accept': 'application/json'},
+                                 json={'query': VotesQuery})
+
+    print(json_response.status_code)
+    print(json_response.text)
